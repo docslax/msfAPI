2bd1c230919237666e64d66426f77bc705882932
DARREL SIEGLE <docslax@gmail.com>
2020-03-02 15:04:53 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+var characters = JSON.parse(fs.readFileSync('characters.min.json', 'utf8'));
+var gear = JSON.parse(fs.readFileSync('gear.min.json', 'utf8'));
+var char_detail = JSON.parse(fs.readFileSync('character_detail.min.json', 'utf8'));
+
+app.get('/character/:charId/:slotId', function (req, res) {
+    res.send(characters[req.params["charId"]][req.params["slotId"]].slots);
+});
+
+app.get('/character/:charId', function( req, res) {
+    res.send(characters[req.params["charId"]]);
+});
+
+app.get('/characters', function (req, res) {
+    var chars = [];
+    for (char in char_detail) {
+        if (parseInt(char_detail[char]["Playable"]) == 0) {
+            chars.push({"name": char_detail[char].Name, "id": char });
+        }
+    }
+    res.send(chars);
+});
+
+app.get('/gear/:gearId/:level?', function (req, res) {
+    var level = req.params.level;
+    if (!level) {
+        res.send(gear[req.params.gearId]);
+    } else {
+        res.send(gear[req.params.gearId][req.params.level]);
+    }
+    //res.send(gear[req.params["gearId"]][req.params["level"]]);
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
